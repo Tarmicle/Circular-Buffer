c3b18ec66b6b666d3830fdc38aba69ee9ef5bab1
Enrico <enrico.bottani@hotmail.com>
2016-12-29 11:06:55 +0100

    #include FIX

--- circular_buffer.c
+++ circular_buffer.c
@@ -3,6 +3,7 @@
 //
 
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include "circular_buffer.h"
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <printf.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "circular_buffer.h"
 
